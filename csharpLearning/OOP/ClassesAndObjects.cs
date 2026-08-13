3966b2a2b2fc343122ddffe22e17aa5916da6639
Vishal Rawat <vishalrawatssr@gmail.com>
2026-08-13 06:53:45 +0530

    Add project files.

---
+++ csharpLearning/OOP/ClassesAndObjects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpLearning.OOP
+{
+    public class ClassesAndObjects
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello I am class");
+        }
+    }
+}
---
+++ csharpLearning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharpLearning
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello Vishal");
+        }
+    }
+}
