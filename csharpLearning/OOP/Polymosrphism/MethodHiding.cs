ef934100180dfb965cca20ec22f88061957ac8a1
Vishal Rawat <vishalrawatssr@gmail.com>
2026-08-29 12:26:11 +0530

    Method OverHiding and shadowing

---
+++ csharpLearning/OOP/Polymosrphism/MethodHiding.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpLearning.OOP.Polymosrphism
+{
+    public class MethodHiding
+    {
+        public void Test()
+        {
+            Console.WriteLine("Hi I am parent method and showing the example of Method over Hiding :");
+        }
+    }
+    public class MethodHiding2 : MethodHiding
+    {
+
+        public new void Test()
+        {
+            Console.WriteLine("Hii I am method of child class");
+        }
+        public void ParentTest()
+        {
+            base.Test();
+        }
+
+        public static void Main()
+        {
+             
+            MethodHiding methodHiding = new MethodHiding2();
+            //methodHiding2.ParentTest();
+            methodHiding.Test();
+        }
+    }
+
+}
---
+++ csharpLearning/OOP/Polymosrphism/MethodOverriding.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpLearning.OOP.Polymosrphism
+{
+    public class MethodOverriding
+    {
+        public virtual void myFunction()
+        {
+            Console.WriteLine("Hi I am parent class method showing the exaple of method overriding: ");
+        }
+    }
+
+    public class SecondClassMethodOverriding : MethodOverriding
+    {
+        public override void myFunction()
+        {
+            Console.WriteLine("Hy I am child class method and I am changing the behaviour of base class");
+        }
+        public static void Main(string[] args)
+        {
+            //MethodOverriding ovr = new MethodOverriding();
+            //ovr.myFunction();
+
+            MethodOverriding s = new SecondClassMethodOverriding();
+            s.myFunction();
+        }
+
+    }
+}
