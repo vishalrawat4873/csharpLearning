adf66bb1b02107ce78246d53c670978822a54320
Vishal Rawat <vishalrawatssr@gmail.com>
2026-08-30 18:57:24 +0530

    Abstraction Practice

---
+++ csharpLearning/OOP/Abstraction/Figure.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace csharpLearning.OOP.Abstraction
+{
+    // Abstract class
+    public abstract class Figure
+    {
+        public double length, width, radius, height, slantHeight;
+
+        public abstract double CalculateArea();
+    }
+
+    // Rectangle
+    public class Rectangle : Figure
+    {
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return length * width;
+        }
+    }
+
+    // Circle
+    public class Circle : Figure
+    {
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    // Triangle
+    public class Triangle : Figure
+    {
+        public Triangle(double length, double height)
+        {
+            this.length = length;
+            this.height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * length * height;
+        }
+    }
+
+    // Cone
+    public class Cone : Figure
+    {
+        public Cone(double radius, double slantHeight)
+        {
+            this.radius = radius;
+            this.slantHeight = slantHeight;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * slantHeight;
+        }
+    }
+
+    // Main class
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(10, 5);
+            Circle circle = new Circle(5);
+            Triangle triangle = new Triangle(10, 5);
+            Cone cone = new Cone(5, 10);
+
+            Console.WriteLine("Rectangle Area: " + rectangle.CalculateArea());
+            Console.WriteLine("Circle Area: " + circle.CalculateArea());
+            Console.WriteLine("Triangle Area: " + triangle.CalculateArea());
+            Console.WriteLine("Cone Area: " + cone.CalculateArea());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharpLearning/OOP/Polymosrphism/OperatorOverLoadingExample/Matrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpLearning.OOP.Polymosrphism.OperatorOverLoadingExample
+{
+    public class Matrix
+    {
+        int a, b ,c ,d ;
+
+        public Matrix(int a, int b, int c, int d)
+        {
+            this.a = a; this.b = b; this.c = c; this.d = d; 
+        }
+        public static Matrix operator +(Matrix obj1, Matrix obj2)
+        {
+            Matrix obj = new Matrix(obj1.a + obj2.a, obj1.b + obj2.b, obj1.c + obj2.c, obj1.d + obj2.d);
+            return obj;
+        }
+    }
+    class TestMatrix
+    {
+        public static void Main()
+        {
+            Matrix m1 = new Matrix(23, 43, 13, 43);
+            Matrix m2 = new Matrix(25, 2, 5, 2);
+            Matrix m3 = m1 + m2;
+        }
+    }
+}
