96c1bc084515a1ea9c070e0f0095ccae55f223e7
Vishal Rawat <vishalrawatssr@gmail.com>
2026-08-31 07:40:52 +0530

    Learn Interface

---
+++ csharpLearning/IInterfaceTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpLearning
+{
+    internal interface IInterfaceTest
+    {
+        void Add(int a, int b);
+    }
+
+    interface IInterfaceTest2 : IInterfaceTest 
+    {
+        void Sub(int a, int b);
+    } 
+    
+    public class ImplementationInterface : IInterfaceTest2
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public static void Main()
+        {
+            ImplementationInterface implementationInterface = new ImplementationInterface();
+            implementationInterface.Add(1, 2);
+            implementationInterface.Sub(1, 2);
+        }
+    }
+}
