d75bfea600f53513ec9e1ca5c0b60f985347b62d
Vishal Rawat <vishalrawatssr@gmail.com>
2026-09-01 08:15:59 +0530

    learn Structure with code

---
+++ csharpLearning/StructureExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace csharpLearning
+{
+    public struct StructureExample
+    {
+        int i;
+        public StructureExample()
+        {
+            
+        }
+        public void Display()
+        {
+            Console.WriteLine("Method in structure"+ i);
+        }
+
+        public static void Main(string[] args) 
+        {
+            StructureExample example = new StructureExample();
+            example.i = 10;
+            example.Display();
+        }
+    }
+}
